fcad6f45f9c694b35a003176a91807cd29b2bc1e
demotuulia <tuulia@live.nl>
2019-03-14 12:48:57 +0100

    Update Test.php

--- Tests/Test.php
+++ Tests/Test.php
@@ -10,7 +10,7 @@
 require_once(__DIR__ . '/../Lib/Core/Autoloader.php');
 require_once(__DIR__ . '/../Config.php');
 
-use PHPUnit\Framework\TestCase;
+use \PHPUnit\Framework\TestCase;
 
 class Test extends TestCase
 {
